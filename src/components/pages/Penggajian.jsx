bef6a02a4d22aa92f902f73be91a051a5360dab4
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-13 22:20:06 +0700

    finish update penggajian page

--- src/components/pages/Penggajian.jsx
+++ src/components/pages/Penggajian.jsx
@@ -1,21 +1,265 @@
-import React, { useState, useEffect } from 'react'
-import { Box } from '@adminjs/design-system'
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  H4,
+  H5,
+  FormGroup,
+  Label,
+  Input,
+  ValueGroup,
+  Button,
+  CurrencyInput
+} from "@adminjs/design-system";
 import { ApiClient, useNotice } from "adminjs";
+import { toFormData } from "axios";
 
 const api = new ApiClient();
 
 const Penggajian = (props) => {
-    useEffect(() => {
-        api.resourceAction({
-            resourceId: "Penggajian",
-            actionName: "detail",
-        }).then((response) => {
-            console.log(response)
+  const [gajiPokok, setGajiPokok] = useState(0);
+  const [tunjanganJabatan, setTunjanganJabatan] = useState(0)
+  const [tunjanganPerusahaan, setTunjanganPerusahaan] = useState(0)
+  const [BPJSKesehatan, setBPJSKesehatan] = useState(0)
+  const [PPH21, setPPH21] = useState(0)
+
+  const [jumlahUangMakan, setJumlahUangMakan] = useState(0)
+  const [nominalUangMakan, setNominalUangMakan] = useState(0)
+  const [subtotalUangMakan, setSubtotalUangMakan] = useState(0)
+
+  const [jumlahUangTransportasi, setJumlahUangTransportasi] = useState(0)
+  const [nominalUangTransportasi, setNominalUangTransportasi] = useState(0)
+  const [subtotalUangTransportasi, setSubtotalUangTransportasi] = useState(0)
+
+  const [jumlahFeeLembur, setJumlahFeeLembur] = useState(0)
+  const [nominalFeeLembur, setNominalFeeLembur] = useState(0)
+  const [subtotalFeeLembur, setSubtotalFeeLembur] = useState(0)
+
+  const [jumlahFeeMCU, setJumlahFeeMCU] = useState(0)
+  const [nominalFeeMCU, setNominalFeeMCU] = useState(0)
+  const [subtotalFeeMCU, setSubtotalFeeMCU] = useState(0)
+
+  const [jumlahPotongan, setJumlahPotongan] = useState(0)
+  const [nominalPotongan, setNominalPotongan] = useState(0)
+  const [subtotalPotongan, setSubtotalPotongan] = useState(0)
+
+  const [totalGaji, setTotalGaji] = useState(0)
+  const [header_id, setHeader_id] = useState(-1)
+
+  useEffect(() => {
+    api
+      .resourceAction({
+        resourceId: "Penggajian",
+        actionName: "detail",
+      })
+      .then((response) => {
+        setHeader_id(response.data.record.populated.header_id);
+        const data = response.data.record.populated.detail;
+        setGajiPokok(val(data, "Gaji Pokok"))
+        setTunjanganJabatan(val(data, "Tunjangan Jabatan"))
+        setTunjanganPerusahaan(val(data, "Tunjangan Perusahaan"))
+        setBPJSKesehatan(val(data, "BPJS Kesehatan"))
+        setPPH21(val(data, "Pajak PPH21"))
+        
+        setJumlahUangMakan(val(data, "Uang Makan", "jumlah"))
+        setNominalUangMakan(val(data, "Uang Makan", "nominal"))
+
+        setJumlahUangTransportasi(val(data, "Uang Transportasi", "jumlah"))
+        setNominalUangTransportasi(val(data, "Uang Transportasi", "nominal"))
+
+        setJumlahFeeLembur(val(data, "Fee Lembur", "jumlah"))
+        setNominalFeeLembur(val(data, "Fee Lembur", "nominal"))
+
+        setJumlahFeeMCU(val(data, "Fee MCU", "jumlah"))
+        setNominalFeeMCU(val(data, "Fee MCU", "nominal"))
+
+        setJumlahPotongan(val(data, "Potongan", "jumlah"))
+        setNominalPotongan(val(data, "Potongan", "nominal"))
+      });
+  }, []);
+
+  useEffect(() => {
+    setSubtotalUangMakan(jumlahUangMakan * nominalUangMakan ?? 0)
+  }, [jumlahUangMakan, nominalUangMakan])
+
+  useEffect(() => {
+    setSubtotalUangTransportasi(jumlahUangTransportasi * nominalUangTransportasi ?? 0)
+  }, [jumlahUangTransportasi, nominalUangTransportasi])
+
+  useEffect(() => {
+    setSubtotalFeeLembur(jumlahFeeLembur * nominalFeeLembur ?? 0)
+  }, [jumlahFeeLembur, nominalFeeLembur])
+
+  useEffect(() => {
+    setSubtotalFeeMCU(jumlahFeeMCU * nominalFeeMCU ?? 0)
+  }, [jumlahFeeMCU, nominalFeeMCU])
+
+  useEffect(() => {
+    setSubtotalPotongan(jumlahPotongan * nominalPotongan ?? 0)
+  }, [jumlahPotongan, nominalPotongan])
+
+  useEffect(() => {
+    setTotalGaji(gajiPokok
+      + tunjanganJabatan
+      + tunjanganPerusahaan
+      + subtotalUangMakan
+      + subtotalUangTransportasi
+      + subtotalFeeLembur
+      + subtotalFeeMCU
+      + BPJSKesehatan
+      + subtotalPotongan
+      - PPH21)
+  })
+
+  const notice = useNotice();
+
+  const val = (d, n, p="subtotal") => {
+    return d.find(x=>x.judul===n)[p]
+  }
+
+  const rp = (n) => {
+    return "Rp "+n.toLocaleString("id-ID")
+  }
+
+  const handleUpdate = () => {
+    api
+      .resourceAction({
+        resourceId: "Penggajian",
+        actionName: "detail",
+        method: "POST",
+        data:toFormData({
+          totalGaji: totalGaji,
+          uangMakan: {
+            jumlah: jumlahUangMakan,
+            nominal: nominalUangMakan,
+            subtotal: subtotalUangMakan,
+          },
+          uangTransportasi: {
+            jumlah: jumlahUangTransportasi,
+            nominal: nominalUangTransportasi,
+            subtotal: subtotalUangTransportasi,
+          },
+          feeLembur: {
+            jumlah: jumlahFeeLembur,
+            nominal: nominalFeeLembur,
+            subtotal: subtotalFeeLembur,
+          },
+          feeMCU: {
+            jumlah: jumlahFeeMCU,
+            nominal: nominalFeeMCU,
+            subtotal: subtotalFeeMCU,
+          },
+          potongan:{
+            jumlah: jumlahPotongan,
+            nominal: nominalPotongan,
+            subtotal: subtotalPotongan,
+          },
+          PPH21: PPH21 * -1,
+          header_id
         })
-    })
-    const notice = useNotice();
+      })
+      .then((response) => {
+        if(response.data == "OK"){
+          notice({message: "Berhasil update"})
+        }
+      })
+  }
 
-    return <Box>Test</Box>
-}
+  return (
+    <Box variant="white">
+      <H4 fontWeight="bold">Gaji {header_id}</H4>
 
-export default Penggajian
+      <ValueGroup label="Gaji Pokok" value={rp(gajiPokok)}/>
+      <ValueGroup label="Tunjangan Jabatan" value={rp(tunjanganJabatan)}/>
+      <ValueGroup label="Tunjangan Perusahaan" value={rp(tunjanganPerusahaan)}/>
+
+      <FormGroup>
+        <Label>Uang Makan</Label>
+        <Box flex={true} alignItems={"baseline"} flexDirection="row">
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(nominalUangMakan)}</Label>
+          <Label fontSize={16} mx={4}> X </Label>
+          <Input mx={4} variant="sm" value={jumlahUangMakan} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setJumlahUangMakan(val)
+          }}/>
+          <Label fontSize={16} mx={4}> = </Label>
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(subtotalUangMakan)}</Label>
+        </Box>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>Uang Transportasi</Label>
+        <Box flex={true} alignItems={"baseline"} flexDirection="row">
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(nominalUangTransportasi)}</Label>
+          <Label fontSize={16} mx={4}> X </Label>
+          <Input mx={4} variant="sm" value={jumlahUangTransportasi} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setJumlahUangTransportasi(val)
+          }}/>
+          <Label fontSize={16} mx={4}> = </Label>
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(subtotalUangTransportasi)}</Label>
+        </Box>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>Fee Lembur</Label>
+        <Box flex={true} alignItems={"baseline"} flexDirection="row">
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(nominalFeeLembur)}</Label>
+          <Label fontSize={16} mx={4}> X </Label>
+          <Input mx={4} variant="sm" value={jumlahFeeLembur} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setJumlahFeeLembur(val)
+          }}/>
+          <Label fontSize={16} mx={4}> = </Label>
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(subtotalFeeLembur)}</Label>
+        </Box>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>Fee MCU</Label>
+        <Box flex={true} alignItems={"baseline"} flexDirection="row">
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(nominalFeeMCU)}</Label>
+          <Label fontSize={16} mx={4}> X </Label>
+          <Input mx={4} variant="sm" value={jumlahFeeMCU} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setJumlahFeeMCU(val)
+          }}/>
+          <Label fontSize={16} mx={4}> = </Label>
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(subtotalFeeMCU)}</Label>
+        </Box>
+      </FormGroup>
+
+      <H4 fontWeight="bold">Potongan</H4>
+
+      <FormGroup>
+        <Label>Potongan Gaji</Label>
+        <Box flex={true} alignItems={"baseline"} flexDirection="row">
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(nominalPotongan)}</Label>
+          <Label fontSize={16} mx={4}> X </Label>
+          <Input mx={4} variant="sm" value={jumlahPotongan} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setJumlahPotongan(val)
+          }}/>
+          <Label fontSize={16} mx={4}> = </Label>
+          <Label fontSize={16} mx={4} fontWeight="bold">{rp(subtotalPotongan)}</Label>
+        </Box>
+      </FormGroup>
+
+      <ValueGroup label="BPJS Kesehatan" value={rp(BPJSKesehatan)}/>
+      <Label>Pajak PPH 21</Label>
+      <CurrencyInput value={PPH21} onChange={(e) => {
+            const val = e.target.value === "" ? 0 : e.target.value
+            setPPH21(parseInt(val.toString().replaceAll(",", "")))
+          }}/>
+
+      <H5 textAlign="right" fontWeight="bold">
+        Total Gaji: {rp(totalGaji)}
+      </H5>
+
+      <Box flex={true} justifyContent="center">
+        <Button align="center" onClick={handleUpdate}>Update</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Penggajian;
--- src/resources/Penggajian.js
+++ src/resources/Penggajian.js
@@ -3,6 +3,7 @@ import { Components } from "../components/index.js";
 import { response } from "express";
 import HPenggajian from "../models/HPenggajian.js";
 import moment from "moment";
+import { raw } from "mysql2";
 
 let data = null;
 
@@ -11,6 +12,7 @@ export default {
   options: {
     editProperties: ['nik','tanggal', 'total'],
     listProperties: ['nik','tanggal', 'total'],
+    filterProperties: ['nik','tanggal'],
     parent: {
       name: "",
       icon: "Money",
@@ -43,15 +45,116 @@ export default {
           else if(request.method == 'post'){
             const payload = request.payload
             const listKaryawan = JSON.parse(payload.karyawan)
-            console.log(listKaryawan)
-            console.log(moment(ddate).format('YYYY-MM-DD'))
+            const constants = await db['Constant'].findAll({
+              raw: true
+            })
+            console.log(constants)
+            // console.log(listKaryawan)
+            // console.log(moment(ddate).format('YYYY-MM-DD'))
             try{
               for (const nik of listKaryawan) {
-                await db["HPenggajian"].create({  
+                const karyawan = await db['Karyawan'].findByPk(nik,{
+                  raw: true
+                })
+                const divisi = await db['Divisi'].findByPk(karyawan.id_divisi,{
+                  raw: true
+                })
+                const header = await db["HPenggajian"].create({  
                   nik: nik,
                   tanggal: moment(ddate).format('YYYY-MM-DD'),
                   total: 0
                 })
+                const jumlah_masuk=30
+                const jumlah_lembur=0
+                const jumlah_mcu=0
+                const header_id = header.dataValues.id
+                await db["DPenggajian"].bulkCreate([
+                  //create gaji pokok
+                  {
+                    id_header: header_id,
+                    judul: "Gaji Pokok",
+                    jumlah: 1,
+                    nominal: constants[0].intvalue,
+                    subtotal: constants[0].intvalue
+                  },
+                  //create tunjangan jabatan
+                  {
+                    id_header: header_id,
+                    judul: "Tunjangan Jabatan",
+                    jumlah: 1,
+                    nominal: divisi.tunjangan,
+                    subtotal: divisi.tunjangan
+                  },
+                  //create tunjangan perusahaan
+                  {
+                    id_header: header_id,
+                    judul: "Tunjangan Perusahaan",
+                    jumlah: 1,
+                    nominal: constants[1].intvalue,
+                    subtotal: constants[1].intvalue
+                  },
+                  //create uang makan
+                  {
+                    id_header: header_id,
+                    judul: "Uang Makan",
+                    jumlah: jumlah_masuk,
+                    nominal: constants[2].intvalue,
+                    subtotal: parseInt(constants[2].intvalue) * jumlah_masuk
+                  },
+                  //create uang transportasi
+                  {
+                    id_header: header_id,
+                    judul: "Uang Transportasi",
+                    jumlah: jumlah_masuk,
+                    nominal: constants[3].intvalue,
+                    subtotal: parseInt(constants[3].intvalue) * jumlah_masuk
+                  },
+                  //create fee lembur
+                  {
+                    id_header: header_id,
+                    judul: "Fee Lembur",
+                    jumlah: jumlah_lembur,
+                    nominal: constants[4].intvalue,
+                    subtotal: parseInt(constants[4].intvalue) * jumlah_lembur
+                  },
+                  //create fee mcu
+                  {
+                    id_header: header_id,
+                    judul: "Fee MCU",
+                    jumlah: jumlah_mcu,
+                    nominal: constants[5].intvalue,
+                    subtotal: parseInt(constants[5].intvalue) * jumlah_mcu
+                  },
+                  //create potongan
+                  {
+                    id_header: header_id,
+                    judul: "Potongan",
+                    jumlah: 0,
+                    nominal: 1000 * -1,
+                    subtotal: 0
+                  },
+                  //create bpjs kesehatan
+                  {
+                    id_header: header_id,
+                    judul: "BPJS Kesehatan",
+                    jumlah: 1,
+                    nominal: constants[6].intvalue * -1,
+                    subtotal: constants[6].intvalue * -1
+                  },
+                  //create pph21
+                  {
+                    id_header: header_id,
+                    judul: "Pajak PPH21",
+                    jumlah: 1,
+                    nominal: 0,
+                    subtotal: 0
+                  }
+                ])
+                // console.log(header)
+              }
+              return {
+                msg: "Success",
+                counter: listKaryawan.length
               }
             }
             catch(e){
@@ -66,16 +169,103 @@ export default {
       detail:{
         actionType:'record',
         component:Components.Penggajian,
-        handler: (request, response, context) => {
+        handler: async (request, response, context) => {
+          if(request.method == "post"){
+            console.log(request.payload)
+            const payload = request.payload
+            await db["DPenggajian"].update({
+              jumlah: payload["uangMakan[jumlah]"],
+              subtotal: payload["uangMakan[subtotal]"]
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Uang Makan"
+              }
+            })
+            await db["DPenggajian"].update({
+              jumlah: payload["uangTransportasi[jumlah]"],
+              subtotal: payload["uangTransportasi[subtotal]"]
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Uang Transportasi"
+              }
+            })
+            await db["DPenggajian"].update({
+              jumlah: payload["feeLembur[jumlah]"],
+              subtotal: payload["feeLembur[subtotal]"]
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Fee Lembur"
+              }
+            })
+            await db["DPenggajian"].update({
+              jumlah: payload["feeMCU[jumlah]"],
+              subtotal: payload["feeMCU[subtotal]"]
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Fee MCU"
+              }
+            })
+            await db["DPenggajian"].update({
+              jumlah: payload["potongan[jumlah]"],
+              subtotal: payload["potongan[subtotal]"]
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Potongan"
+              }
+            })
+            await db["DPenggajian"].update({
+              nominal: payload.PPH21,
+              subtotal: payload.PPH21
+            },{
+              where:{
+                id_header: payload.header_id,
+                judul: "Pajak PPH21"
+              }
+            })
+
+            await db["HPenggajian"].update({
+              total: payload.totalGaji
+            },{
+              where:{
+                id: payload.header_id
+              }
+            })
+
+            return "OK"
+          }
           const { record, currentAdmin } = context
           if(record != null){
             data = record
           }
+          const detail = await db["DPenggajian"].findAll({
+            where:{
+              id_header: data.params.id
+            },
+            raw: true
+          })
+          
+          data.populated.detail = detail
+          data.populated.header_id = data.params.id
+          // console.log(data)
           return {
             record: data.toJSON(currentAdmin),
           }
         },
       },
+      show:{
+        isVisible: false,
+      },
+      create:{
+        isVisible: false,
+      },
+      edit:{
+        isVisible: false,
+      }
     }
   },
 };
