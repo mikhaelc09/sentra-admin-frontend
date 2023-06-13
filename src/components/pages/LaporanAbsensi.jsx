742ea4b26965d4fbc49f9a81f1cc5ee3519560bc
mikhaelc09 <mikhael.chris09@gmail.com>
2023-06-14 02:07:24 +0700

    add field for other report

--- src/components/pages/LaporanAbsensi.jsx
+++ src/components/pages/LaporanAbsensi.jsx
@@ -4,7 +4,9 @@ import { Box, H4, Label, Button } from "@adminjs/design-system";
 import { toFormData } from "axios";
 
 const LaporanAbsensi = (props) => {
-  const [month, setMonth] = useState('2023-06');
+  const [month1, setMonth1] = useState('2023-06');
+  const [month2, setMonth2] = useState('2023-06');
+  const [month3, setMonth3] = useState('2023-06');
   const api = new ApiClient();
 
   const generateAbsensi = () => {
@@ -28,16 +30,50 @@ const LaporanAbsensi = (props) => {
   return (
     <Box variant="white">
       <H4>Generate Laporan</H4>
-      <Box flex={true} justifyContent={"space-between"} alignItems="baseline">
+      <Box mb={12} flex={true} justifyContent={"space-between"} alignItems="baseline">
         <Box flex={true} alignItems={"baseline"}>
           <Label mr={12} fontSize={16}>
             Laporan Absensi
           </Label>
           <input
             type="month"
-            value={month}
+            value={month1}
             onChange={(e) => {
-              setMonth(e.target.value);
+              setMonth1(e.target.value);
+            }}
+          />
+        </Box>
+        <Button onClick={generateAbsensi} variant="primary">
+          Generate
+        </Button>
+      </Box>
+      <Box mb={12} flex={true} justifyContent={"space-between"} alignItems="baseline">
+        <Box flex={true} alignItems={"baseline"}>
+          <Label mr={12} fontSize={16}>
+            Laporan Penggajian
+          </Label>
+          <input
+            type="month"
+            value={month2}
+            onChange={(e) => {
+              setMonth2(e.target.value);
+            }}
+          />
+        </Box>
+        <Button onClick={generateAbsensi} variant="primary">
+          Generate
+        </Button>
+      </Box>
+      <Box mb={12} flex={true} justifyContent={"space-between"} alignItems="baseline">
+        <Box flex={true} alignItems={"baseline"}>
+          <Label mr={12} fontSize={16}>
+            Laporan MCU
+          </Label>
+          <input
+            type="month"
+            value={month3}
+            onChange={(e) => {
+              setMonth3(e.target.value);
             }}
           />
         </Box>
