d09e423b2384dd2d728f6980c5af72b5c2286e19
mikhaelc09 <mikhael.chris09@gmail.com>
2023-07-06 15:07:24 +0700

    enumerate hari value on Master Jadwal

--- src/resources/Jadwal.js
+++ src/resources/Jadwal.js
@@ -12,5 +12,18 @@ export default {
     showProperties: ["nik", "id_lokasi", "hari", "jam_kerja"],
     editProperties: ["nik", "id_lokasi", "hari", "jam_kerja"],
     filterProperties: ["nik", "id_lokasi", "hari", "jam_kerja"],
+    properties:{
+      hari:{
+        availableValues: [
+          { value: "SENIN", label: "SENIN" },
+          { value: "SELASA", label: "SELASA" },
+          { value: "RABU", label: "RABU" },
+          { value: "KAMIS", label: "KAMIS" },
+          { value: "JUMAT", label: "JUMAT" },
+          { value: "SABTU", label: "SABTU" },
+          { value: "MINGGU", label: "MINGGU" },
+        ],
+      }
+    },
   },
 };
