9ece402a5f3f994c2ed503a9b2833d9ce43ba013
mikhaelc09 <mikhael.chris09@gmail.com>
2023-12-07 08:46:30 +0700

    fix: update karyawan resource field

--- src/resources/Karyawan.js
+++ src/resources/Karyawan.js
@@ -11,8 +11,8 @@ export default {
     },
     listProperties: ["nik", "nama", "email", "status"],
     filterProperties: ["nik", "nama", "email", "status"],
-    editProperties: ["nik","nama","id_divisi","email","no_telp","alamat","nama_rekening","no_rekening","keterangan","status","gaji_pokok", "tunjangan_perusahaan"],
-    showProperties: ["nik","nama","id_divisi","email","no_telp","alamat","nama_rekening","no_rekening","keterangan","tabel_jamkerja","gaji_pokok", "tunjangan_perusahaan"],
+    editProperties: ["nik","nama","id_divisi","email","no_telp","nama_rekening","no_rekening","keterangan","status","gaji_pokok", "tunjangan_perusahaan"],
+    showProperties: ["nik","nama","id_divisi","email","no_telp","nama_rekening","no_rekening","keterangan","tabel_jamkerja","gaji_pokok", "tunjangan_perusahaan"],
     actions: {
       show: { after: [getJadwal()] },
     },
